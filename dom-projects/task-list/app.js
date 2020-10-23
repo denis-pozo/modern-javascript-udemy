6a4336283f84e31235ba4781a07b46c73c29146a
Denis Pozo <denispzpd@gmail.com>
2020-10-23 08:19:20 +0200

    Add event listener for create, remove and filter task

---
+++ dom-projects/task-list/app.js
@@ -0,0 +1,65 @@
+// Define UI Vars
+// Remember '#' for ids and '.' for classes
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
+}
+
+function addTask(event) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+  taskInput.value = '';
+
+  event.preventDefault();
+}
+
+function removeTask(event) {
+  if(event.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')){
+      event.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(event) {
+  const text = event.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    function(task) {
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    });
+} 
+
