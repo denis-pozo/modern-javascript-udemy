7c897c05d9f7a042c2070921a82ba0b0869d40b1
Denis Pozo <denispzpd@gmail.com>
2020-11-08 19:24:51 +0100

    Add remove book logic

--- book-list/app.js
+++ book-list/app.js
@@ -33,6 +33,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+UI.prototype.deleteBook = function(target) {
+  
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
@@ -54,6 +61,14 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   }
 
   ui.clearFields();
+
+  e.preventDefault();
+});
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+  const ui = new UI();
+  ui.deleteBook(e.target);
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
